5178a2d1c590775132a0285fec1c33e845e5e0e9
aseemsavio <aseemsavio3@gmail.com>
2022-03-19 11:25:53 +0530

    db changes

--- utils/database.go
+++ utils/database.go
@@ -2,8 +2,6 @@ package utils
 
 import (
 	"context"
-	"fmt"
-	"google.golang.org/api/googleapi"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 	"log"
@@ -25,47 +23,6 @@ func DatabaseService() (Database, error) {
 		log.Fatalf("Unable to retrieve Sheets Client %v", error)
 	}
 	return Database{service: service}, nil
-}
-
-func (db *Database) GetSpreadSheet(sheetId string) *sheets.SpreadsheetsGetCall {
-	return db.service.Spreadsheets.Get(sheetId)
-}
-
-func SpreadSheet() sheets.Spreadsheet {
-	return sheets.Spreadsheet{
-		DataSourceSchedules: nil,
-		DataSources:         nil,
-		DeveloperMetadata:   nil,
-		NamedRanges:         nil,
-		Properties: &sheets.SpreadsheetProperties{
-			AutoRecalc:                   "",
-			DefaultFormat:                nil,
-			IterativeCalculationSettings: nil,
-			Locale:                       "",
-			SpreadsheetTheme:             nil,
-			TimeZone:                     "",
-			Title:                        "Aseem's Finances",
-			ForceSendFields:              nil,
-			NullFields:                   nil,
-		},
-		Sheets:          nil,
-		SpreadsheetId:   "",
-		SpreadsheetUrl:  "",
-		ServerResponse:  googleapi.ServerResponse{},
-		ForceSendFields: nil,
-		NullFields:      nil,
-	}
-}
-
-// CreateDatabase creates a spreadsheet to be used as a database.
-func (db *Database) CreateDatabase(spreadSheet *sheets.Spreadsheet) {
-	res, error := db.service.Spreadsheets.Create(spreadSheet).Context(context.Background()).Do()
-	if error != nil {
-		fmt.Printf("Error occurred: %#v\n", error)
-	} else {
-		fmt.Println(res.SpreadsheetId, res.HTTPStatusCode, res.Properties, res.Sheets)
-		fmt.Printf("%#v\n", res)
-	}
 }
 
 // AddData adds data to the database.
